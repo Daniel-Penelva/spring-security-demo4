801b8c0935052fee7d5db553238ab4e154e9b6e8
Daniel Penelva <d4n.andrade@gmail.com>
2024-04-25 02:37:07 -0300

    criando a classe OurUserInfoUserDetailsService implementando UserDetailsService

---
+++ src/main/java/com/apirest/springsecuritydemo4/config/OurUserInfoUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.apirest.springsecuritydemo4.config;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.apirest.springsecuritydemo4.entity.OurUser;
+import com.apirest.springsecuritydemo4.repository.OurUserRepository;
+
+@Configuration
+public class OurUserInfoUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private OurUserRepository ourUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<OurUser> user = ourUserRepository.findByEmail(username);
+        return user.map(OurUserInfoDetails::new).orElseThrow(() -> new UsernameNotFoundException("Usuário não existe!"));
+    }
+
+}
